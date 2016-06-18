ecb3e7f04876522a066ca0f6a5a383b72c21b9a2
Bill Langley <brainstormwilly@gmail.com>
2016-06-17 23:55:45 -0700

    another hot fix for heroku mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'arcane-atoll-89530.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    address: "smtp.sendgrid.net",
+    port: 2525,
+    domain: "heroku.com",
     authentication: "plain",
     enable_starttls_auto:true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
   }
 
 end
