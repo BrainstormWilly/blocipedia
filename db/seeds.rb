85df7bcd2fe99e1a29489e1ebb558d072b994257
Bill Langley <brainstormwilly@gmail.com>
2016-06-22 22:29:01 -0700

    Checkpoint 6 Seeding Data: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
-# require "random_data"
-#
-# member = User.create( name: "Willy", email: "brainstormwilly@gmail.com", password: "123456" )
-# member.confirm
-#
-# 10.times do
-#
-# end
+require "random_data"
+
+5.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: RandomData.random_sentence)
+  user.confirm
+end
+users = User.all
+
+admin = User.create(name: "Willy", email: "brainstormwilly@gmail.com", password: "123456", role: "admin")
+admin.confirm
+
+member = User.create(name: "YnoGuy", email: "bill@ynoguy.com", password: "123456")
+member.confirm
+
+20.times do
+  Wiki.create(title: Faker::Lorem.word, body: Faker::Lorem.paragraph, user: users.sample)
+end
+
+puts "Seeds Finished."
+puts "#{User.count} users."
+puts "#{Wiki.count} wikis."
